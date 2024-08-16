d74e3d36e72247db23a9f7fa29d013d45f6eeb9f
rishikesh <malleswaraws@gmail.com>
2024-08-16 21:18:59 +0530

    hello script

--- 04-variables.sh
+++ 04-variables.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #declaring variable
-PERSON1=Rishikesh #no space between = and valuve
-PERSON2=Anvika
+PERSON1=$1 #no space between = and valuve
+PERSON2=$2
 
 #refering variable
 echo "$PERSON1:: Hello $PERSON2, How are you"
