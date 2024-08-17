1d7ecee172ffd5a3c5d04ea7a4f9de3cc7b1a583
rishikesh <malleswaraws@gmail.com>
2024-08-17 20:36:43 +0530

    hello script

--- 03-variables.sh
+++ 03-variables.sh
@@ -6,6 +6,6 @@ PERSON2=Anvika
 
 #refering variable
 echo "$PERSON1:: Hello $PERSON2, How are you"
-echo "$PERSON2:: Hi $PERSON1, I am fine. How are you"
+echo "{$PERSON2}:: Hi $PERSON1, I am fine. How are you"
 echo "$PERSON1: I am fine too, How is your work"
 echo "$PERSON2:: not bad. I am thinking to upgrade to DevOps"
