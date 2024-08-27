685bc17c353012640cc7c26cac3bb88397a47e13
rishikesh <malleswaraws@gmail.com>
2024-08-27 22:27:37 +0530

    hello script

--- 12-colors.sh
+++ 12-colors.sh
@@ -19,6 +19,7 @@ VALIDATE(){
         echo -e "$2...$G SUCCESS $N"
     fi 
 }
+
 if  [ $USERID -ne 0 ]
 then
     echo "plase run this script with root access."
@@ -34,4 +35,4 @@ dnf install git -y &>>$LOGFILE
 VALIDATE $? "Installing Git"
 
 dnf install dockerr -y &>>$LOGFILE
-VALIDATE $? "Installing Docker"
+VALIDATE $? "Installing Docker
