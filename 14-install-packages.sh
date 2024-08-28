ae348d473e43a1d72c4161b0ec41e31df12772b6
rishikesh <malleswaraws@gmail.com>
2024-08-28 23:06:02 +0530

    hello script

--- 14-install-packages.sh
+++ 14-install-packages.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 
 USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2...$R FAILURE $N"
+        exit 1
+    else
+        echo -e "$2...$G SUCCESS $N"
+    fi 
+}
+
 if  [ $USERID -ne 0 ]
 then
     echo "plase run this script with root access."
@@ -12,4 +26,11 @@ fi
 for i in $@
 do 
    echo "package to install: $i"
+   dnf list installed $i &>>$LOGFILE
+   fi [ $? -eq 0 ]
+   then
+       echo "$I already installed...SKIPPING"
+    else
+        echo "$i not installed...Need to install"
+    fi
 done 
