6b0edf8f358a95d6f11c8bb32d95fc1ce542f387
rishikesh <malleswaraws@gmail.com>
2024-09-04 22:15:08 +0530

    functions

---
+++ 15-current-script.sh
@@ -0,0 +1,10 @@
+#!?/bin/bash
+
+COURSE="DevOps from current Script"
+
+echo "Before calling other script, course: $course"
+echo "process ID OF current shell script: $$"
+
+#./16-other-script.shell
+
+echo "After calling other script, course: $COURSE"
