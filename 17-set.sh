c37da5b6118b0cf94fdb1f671fecb2ce722b5951
rishikesh <malleswaraws@gmail.com>
2024-09-07 16:07:09 +0530

    functions

---
+++ 17-set.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+failure(){
+    echo "Failed at $1: $2"
+}
+trap 'failure ${LINENO} "$BASH_COMMAND"' ERR
+
+USERID=$(id -u) #ERR
+
+if  [ $USERID -ne 0 ]
+then
+    echo "plase run this script with root access."
+    exit 1 # manually exit if error comes.
+else
+    echo "you are super user."
+fi
+
+dnf install mysqleee -y
+dnf install git -y
+
+echo "is script proceeding?"
