6146b4d2fe43e75b926fab7a8be9c17b09d9b3c6
rishikesh <malleswaraws@gmail.com>
2024-09-16 20:28:10 +0530

    functions

--- 19-disk-usage.sh
+++ 19-disk-usage.sh
@@ -9,6 +9,8 @@ do
     FOLDER=$(echo $line | awk -F " " '{print $NF}')
     if [ $USAGE -ge $DISK_THRESHOLD ]
     then
-        echo "$FOLDER is more than $DISK_THRESHOLD, current usage: $USAGE"
+        MESSAGE="$FOLDER is more than $DISK_THRESHOLD, current usage: $USAGE"
     fi    
 done <<< $DISK_USAGE
+
+echo "Message: $MESSAGE"
